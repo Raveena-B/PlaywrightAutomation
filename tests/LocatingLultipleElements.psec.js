976629c35392ea969d38db946b9d7bcf62323e97
RAVEENA\ravee <raveena@creatit.com.au>
2025-06-26 16:59:09 +0530

    updated

---
+++ tests/LocatingLultipleElements.psec.js
@@ -0,0 +1,7 @@
+const {test,expect} = require('@playwright/test')
+
+test('LocateMultipleElements',async ({page})=>{
+
+    page.goto('https://www.orivet.com/')
+    
+})
--- tests/Locators.spec.js
+++ tests/Locators.spec.js
@@ -1,7 +1,17 @@
-const{test,expect} = require('playwright')
+// @ts-check
+import { test, expect } from '@playwright/test';
 
-test('Locaors'),async ({page}) => {
 
-    page.goto("https://www.orivet.com/login")
-    
-}
+test('Login test with locators', async ({ page }) => {
+  await page.goto('https://www.orivet.com/login');
+
+  await page.click("//a[normalize-space()='Login']");
+  await page.fill("input[id='email']", 'Charlie');
+  await page.fill("input[id='password']", 'Charlie2017');
+  await page.click("//button[normalize-space()='Login']");
+
+  const logoutLink = page.locator("//button[normalize-space()='Logout']");
+  await expect(logoutLink).toBeVisible();
+
+  await page.close()
+});
