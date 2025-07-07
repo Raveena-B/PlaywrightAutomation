d789ba26fae3927d9ef9ca5cc91466c0062e8ad6
RAVEENA\ravee <raveena@creatit.com.au>
2025-07-07 14:22:24 +0530

    builtin locators update

--- tests/Locators_builtin.spec.js
+++ tests/Locators_builtin.spec.js
@@ -4,8 +4,15 @@ test('Built-inLocators' , async({page}) => {
 
     await page.goto("https://www.orivet.com/login")
 
+
+    //getByAltText() - 
     const logoImage = await page.getByAltText('dog-cat-img')
     await expect(logoImage).toBeVisible();
 
+    //getByPlaceholder()
+    await page.getByPlaceholder("Username").fill("Charlie")
+    await page.getByPlaceholder("Password").fill("Charlie2017")
+
+    //getByRole() - to locate implicit and explicit attributes
     
 })
